5c4d031ee2bf041274c5bf889f2e3477c7253adc
pavani <pavaniavni456@gmail.com>
2022-02-10 11:33:00 +0530

    added thread

---
+++ Thread/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Thread th = Thread.CurrentThread;
+            Console.WriteLine(th);
+        }
+    }
+}
+
